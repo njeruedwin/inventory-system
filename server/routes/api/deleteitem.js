50d8de949e162d9152295c33deb0278d28412cba
Edwin Mugambi <edwinnjeru77@gmail.com>
2023-05-31 11:33:57 +0300

    :artist: solved callback depracation

--- server/routes/api/deleteitem.js
+++ server/routes/api/deleteitem.js
@@ -4,15 +4,16 @@ const router = express.Router();
 const Item = require("../../models/Item");
 
 router.delete("/", (req, res) => {
-  Item.deleteOne({ _id: req.query.id }, (err, doc) => {
-    if (err) {
+  Item.deleteOne({ _id: req.query.id })
+    .then((doc) => {
+      res.send(doc);
+    })
+    .catch((err) => {
       return res.send({
         success: false,
         message: "Server Error",
       });
-    }
-    res.send(doc);
-  });
+    });
 });
 
 module.exports = router;
